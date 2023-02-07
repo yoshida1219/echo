bacfcf020dff838502e1e32657cbf75447e69500
TsuchidaSK <kt312tk312@gmail.com>
2023-02-07 12:33:23 +0900

    画像アップロード修正2

--- target/classes/static/js/share_response.js
+++
@@ -1,9 +0,0 @@
-function share_response() {
-    let paramUrl = "/Hiroba/share_response";
-    
-    $.ajax({
-        type: "GET",
-        url : paramUrl,
-        data: {response_creater : $("#response_creater").val(), response_id : $("#response_id").val(), url : $("#movieID_html").val()}
-    })
-}
