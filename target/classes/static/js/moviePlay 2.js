0c729634bd865756e3a18ddbf5d8f16e7c060725
shunsocial0130 <awbwewww@gmail>
2023-02-10 21:18:26 +0900

    新しいDBです。インポートお願いします

--- target/classes/static/js/moviePlay 2.js
+++
@@ -1,6 +0,0 @@
-function removeFeatureShare(input) {
-    var inputValue = input.value;
-    if (inputValue.includes("?feature=share")) {
-      input.value = inputValue.replace("?feature=share", "");
-    }
-  }
