ad93ed8ec7c12583320230c07fef65cf660d1021
TsuchidaSK <kt312tk312@gmail.com>
2023-02-11 16:11:08 +0900

    動画詳細修正

--- src/main/resources/static/js/throw.js
+++ src/main/resources/static/js/throw.js
@@ -1,23 +1,2 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//   document.getElementById("url-pc-thread").addEventListener("input", function() {
-//     alert("error");
-//     var inputValue = document.getElementById("url-pc-thread").value;
-//     if (inputValue.includes("https://www.youtube.com/@")) {
-//       alert("error2");
-//     }
-//   });
-// });
 
-
-
-
-  // document.getElementById("url-pc-thread").addEventListener("input", function() {
-  //   alert("error");
-  //   var inputValue = document.getElementById("url-pc-thread").value;
-  //   if (inputValue.includes("https://www.youtube.com/@")) {
-  //     alert("error2");
-  //   }
-  // });
-  
-  
     
