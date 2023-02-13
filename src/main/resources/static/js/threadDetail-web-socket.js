e50ca43bda5e65487a85c64b5933f5762af6d789
yoshida1219 <101161712+yoshida1219@users.noreply.github.com>
2023-02-14 00:20:13 +0900

    投稿にゴミ箱追加

--- src/main/resources/static/js/threadDetail-web-socket.js
+++ src/main/resources/static/js/threadDetail-web-socket.js
@@ -80,7 +80,14 @@ function showResponse(response) {
                             <div class="come come2">
                                 <a
                                     href="/Hiroba/delete_response/ThreadDetail?response_creater=` + response.response_creater + `&response_id=` + response.response_id + `&thread_id=` + response.thread_id + `">
-                                    <span class="jaf">削除する</span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trash" width="35" height="35" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                        <path d="M4 7l16 0"></path>
+                                        <path d="M10 11l0 6"></path>
+                                        <path d="M14 11l0 6"></path>
+                                        <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12"></path>
+                                        <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3"></path>
+                                    </svg>
                                 </a>
                             </div>
                         </div>
